a60fa904568c487b05546e7860d9ea74aee9770c
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-01-29 18:05:25 -0700

    final

--- lesson4/js/script4.js
+++ lesson4/js/script4.js
@@ -1,3 +1,37 @@
+let dayname = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+let monts = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Nomberber",
+    "December",
+];
+let d = new Date();
+let dName = dayname[d.getDay()];
+let dMonth = monts[d.getMonth()];
+let fulldate = dName + ", " + d.getDate() + " " + dMonth + " " + d.getFullYear();
+document.getElementById("currentdate").textContent = fulldate;
+
+
+
+
+/*
 window.addEventListener('load', (event)=>{
     const lu = document.querySelector('#currentdate');
     lu.textContent = document.lastModified;
@@ -5,7 +39,7 @@ window.addEventListener('load', (event)=>{
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
 })
-
+*/
 function toggleMenu() {
    document.getElementById('primariNav').classList.toggle('hide');
 }
