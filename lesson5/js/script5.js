7363af88a4d1ecea04c9e9248617704516dd58ed
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-02-04 09:34:55 -0700

    blah

--- lesson5/js/script5.js
+++ lesson5/js/script5.js
@@ -28,10 +28,6 @@ let dMonth = monts[d.getMonth()];
 let fulldate = dName + ", " + d.getDate() + " " + dMonth + " " + d.getFullYear();
 document.getElementById("currentdate").textContent = fulldate;
 
-
-
-
-/*
 window.addEventListener('load', (event)=>{
     const lu = document.querySelector('#currentdate');
     lu.textContent = document.lastModified;
@@ -39,7 +35,21 @@ window.addEventListener('load', (event)=>{
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
 })
-*/
+
 function toggleMenu() {
    document.getElementById('primariNav').classList.toggle('hide');
 }
+
+
+/*
+function pancake();
+    pancakeElement = document.getElementById('pancake');
+    const formElement = document.form[0];
+    formElement.addEventListener('submit', function(e) {
+    e.preventDefault();
+    pancakeElement.innerHTML = 'Hello Today is Friday Pancake friday!'
+    pancakeElement.style.display = 'block';
+    document.body.classList.toggle('moveDown')
+})
+*/
+
