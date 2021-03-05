7aca993578358e69be56858d618bf87d4e9ad3c5
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-03-04 17:47:41 -0700

    final

--- lesson9/home-page3.js
+++ lesson9/home-page3.js
@@ -92,7 +92,7 @@ fetch(requestURL)
         div_1.appendChild(fish_rain);
         fish_popu.textContent = 'Current Population: ' + towns[1].currentPopulation;
         div_1.appendChild(fish_popu);
-        img_fish.setAttribute('src', towns[1].photo, 'alt', towns[1].name + ' photo');
+        img_fish.setAttribute('src','../lesson9/images/photo1.2.jpg', 'alt', towns[1].name + ' photo');
         div_photo1.appendChild(img_fish);
         img_fish.setAttribute('alt', towns[1].name + ' photo');
         div_photo1.appendChild(img_fish);
@@ -112,7 +112,7 @@ fetch(requestURL)
         div_2.appendChild(preston_rain);
         preston_popu.textContent = 'Current Population: ' + towns[5].currentPopulation;
         div_2.appendChild(preston_popu);
-        img_preston.setAttribute('src', towns[5].photo, 'alt', towns[5].name + ' photo');
+        img_preston.setAttribute('src', '../lesson9/images/photo1.1.jpg', 'alt', towns[5].name + ' photo');
         div_photo2.appendChild(img_preston);
         img_preston.setAttribute('alt', towns[5].name + ' photo');
         div_photo2.appendChild(img_preston);
@@ -134,7 +134,7 @@ fetch(requestURL)
         div_3.appendChild(soda_rain);
         soda_popu.textContent = 'Current Population: ' + towns[6].currentPopulation;
         div_3.appendChild(soda_popu);
-        img_soda.setAttribute('src', towns[6].photo, 'alt', towns[6].name + ' photo');
+        img_soda.setAttribute('src', '../lesson9/images/photo1.3.jpg', towns[6].name + ' photo');
         div_photo3.appendChild(img_soda);
         img_soda.setAttribute('alt', towns[6].name + ' photo');
         div_photo3.appendChild(img_soda);
@@ -145,6 +145,27 @@ fetch(requestURL)
         document.querySelector('div.cities').appendChild(card);
         document.querySelector('section').appendChild(div_1);
         document.querySelector('section').appendChild(div_photo1);
+
+        
+        // function alternate(size, size2){
+            
+        //     if(size.matches || size2.matches){
+        //         document.querySelector('div.cities2').appendChild(card2);
+        //         document.querySelector('div.cities2 section').appendChild(div_photo2);
+        //         document.querySelector('div.cities2 section').appendChild(div_2);
+        //     }
+        //     else{
+        //         document.querySelector('div.cities2').appendChild(card2);
+        //         document.querySelector('div.cities2 section').appendChild(div_2);
+        //         document.querySelector('div.cities2 section').appendChild(div_photo2);
+        //     }
+        // }
+        // let size = window.matchMedia('(min-width: 772px)')
+        // let size2 = window.matchMedia('(max-width: 1049px)')
+        // alternate(size, size2)
+        // size.addEventListener(size,size2)
+        // size2.addEventListener(size,size2)
+
 
         document.querySelector('div.cities2').appendChild(card2);
         document.querySelector('div.cities2 section').appendChild(div_2);
