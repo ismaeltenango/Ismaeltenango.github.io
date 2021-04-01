6c8f402651ca931fc6ed09e30bbf2f1f8ed60709
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-03-31 19:45:42 -0600

    final3.0

--- zfinal/homepage/js/home_page.js
+++ zfinal/homepage/js/home_page.js
@@ -22,13 +22,41 @@ const navSlide= () => {
 
         //quitar lineas por una x
         burger.classList.toggle('toggle');
-    });
-
-    
-    
+    }); 
 }
 navSlide();
 
+window.addEventListener('load', (event)=>{
+    const images = document.querySelectorAll("[data-src]");
+
+    function preloadImage(img){
+        console.log('trying to load '+img);
+        const src= img.getAttribute('data-src');
+        if(!src){
+            return;
+        }
+        img.src= src;
+    }
+    const imgOptions = {
+        threshold:0.9999,
+        rootMargin: '0px 0px -200px 0px'
+    }
+    const imgObserver = new IntersectionObserver( (entries, imgObserver)=> {
+        console.log(entries);
+        entries.forEach(entry=>{
+            if(!entry.isIntersecting){
+                return;
+            }
+            else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        })
+    },imgOptions);
+    images.forEach(image=>{
+        imgObserver.observe(image);
+    });
+});
 
 
 
